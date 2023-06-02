4e0194aa7e6aa3413faa05b96eae5f11f15ab9fe
Kshitij Arya <kshitijcrusader36@gmail.com>
2023-06-03 01:02:31 +0530

    Light and dark mode toggle added

---
+++ script.js
@@ -0,0 +1,12 @@
+const BODY = document.body;
+const BUTTON = document.querySelector("button");
+const SYNC = document.querySelector("#sync");
+
+const TOGGLE = () => {
+  const IS_PRESSED = BUTTON.matches("[aria-pressed='true']");
+  if (SYNC.checked)
+    document.body.setAttribute("data-dark-mode", IS_PRESSED ? "false" : "true");
+  BUTTON.setAttribute("aria-pressed", IS_PRESSED ? "false" : "true");
+};
+
+BUTTON.addEventListener("click", TOGGLE);
