7a952f63bf19fcbf8f991049de05f96b0110ceba
Kshitij Arya <kshitijcrusader36@gmail.com>
2023-06-10 23:52:01 +0530

    cleaned up some code

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const slider = document.getElementById("switch");
-const body = document.body;
 
-slider.addEventListener("input", () => {
-    body.classList.toggle("dark", slider.value == "1");
-});
