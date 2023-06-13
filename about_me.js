2391e4e357e6b4c70ce1364487205c82c7a18102
Kshitij Arya <kshitijcrusader36@gmail.com>
2023-06-13 11:43:53 +0530

    code refactored

--- about_me.js
+++ about_me.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-    $(".title").animate({ opacity: 1 }, 1000);
-    $(".profile-picture").delay(500).animate({ opacity: 1 }, 1000);
-    $(".name").delay(1000).animate({ opacity: 1 }, 1000);
-    $(".description").delay(1500).animate({ opacity: 1 }, 1000);
-    $(".projects-title").delay(2000).animate({ opacity: 1 }, 1000);
-    $(".skills-title").delay(2500).animate({ opacity: 1 }, 1000);
-    $(".kanban-board").delay(2700).animate({ opacity: 1 }, 1000);
-  });
+$(document).ready(function () {
+  $(".title").animate({ opacity: 1 }, 1000);
+  $(".profile-picture").delay(500).animate({ opacity: 1 }, 1000);
+  $(".name").delay(1000).animate({ opacity: 1 }, 1000);
+  $(".description").delay(1500).animate({ opacity: 1 }, 1000);
+  $(".projects-title").delay(2000).animate({ opacity: 1 }, 1000);
+  $(".skills-title").delay(2500).animate({ opacity: 1 }, 1000);
+  $(".kanban-board").delay(2700).animate({ opacity: 1 }, 1000);
+});
