c1b59b13a5f01a44cc24c1a38b0eedbbcb44fae6
Kshitij Arya <kshitijcrusader36@gmail.com>
2023-06-28 15:27:45 +0530

    header made universal

---
+++ header.js
@@ -0,0 +1,11 @@
+let toggle = document.getElementById('toggle');
+
+toggle.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+  } else {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+  }
+});
