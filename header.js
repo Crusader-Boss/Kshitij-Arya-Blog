84282ea5b03470c0a57c3081bdd869907c053fa4
Kshitij Arya <kshitijcrusader36@gmail.com>
2023-07-05 16:34:09 +0530

    code reformatted

--- header.js
+++ header.js
@@ -1,11 +1,11 @@
-let toggle = document.getElementById('toggle');
+let toggle = document.getElementById("toggle");
 
-toggle.addEventListener('change', (event) => {
+toggle.addEventListener("change", (event) => {
   if (event.target.checked) {
-    document.body.style.backgroundColor = 'black';
-    document.body.style.color = 'white';
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
   } else {
-    document.body.style.backgroundColor = 'white';
-    document.body.style.color = 'black';
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
   }
 });
