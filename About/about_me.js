8d5c2e9dda0dcb23af8736aa3ebdb1cff57f6bea
Kshitij Arya <kshitijcrusader36@gmail.com>
2023-07-30 14:59:46 +0530

    code cleaned

--- About/about_me.js
+++ About/about_me.js
@@ -3,6 +3,4 @@ $(document).ready(function () {
   $(".profile-picture").delay(500).animate({ opacity: 1 }, 1000);
   $(".name").delay(1000).animate({ opacity: 1 }, 1000);
   $(".description").delay(1500).animate({ opacity: 1 }, 1000);
-  $(".skills-title").delay(2500).animate({ opacity: 1 }, 1000);
-  $(".kanban-board").delay(2700).animate({ opacity: 1 }, 1000);
 });
